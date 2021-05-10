eda46150b2406c05a364d6ada5365a94ff1b525a
sreya-kata <sreya.kata@gmail.com>
2021-05-10 19:18:03 -0400

    different version of socials

--- src/components/whats-happening/social-media/social-media.js
+++ src/components/whats-happening/social-media/social-media.js
@@ -1,38 +1,37 @@
 import React from "react"
-import InstagramEmbed from "react-instagram-embed"
-import { FacebookProvider, Page } from "react-facebook"
 import {
   SocialMediaSectionContainer,
   SocialMediaContainer,
-  Social,
   SocialTitle,
 } from "./social-media-styles"
 
 const SocialMedia = ({ instaLink, facebookPage, title }) => {
+  const embedLink = "https://www.instagram.com/p/COf2vGdFalz/embed"
+
   return (
     <SocialMediaSectionContainer>
       <SocialTitle>{title}</SocialTitle>
       <SocialMediaContainer>
-        <Social>
-          <InstagramEmbed
-            url={instaLink}
-            clientAccessToken={process.env.FACEBOOK_CLIENT_ACCESS_TOKEN}
-            maxWidth={320}
-            hideCaption={true}
-            containerTagName="div"
-            protocol=""
-            injectScript
-            onLoading={() => {}}
-            onSuccess={() => {}}
-            onAfterRender={() => {}}
-            onFailure={() => {}}
-          />
-        </Social>
-        <Social>
-          <FacebookProvider appId={process.env.FACEBOOK_APP_ID}>
-            <Page href={facebookPage} tabs="timeline" />
-          </FacebookProvider>
-        </Social>
+        <iframe
+          title="Instagram"
+          src={embedLink}
+          width="400"
+          height="480"
+          frameborder="0"
+          scrolling="no"
+          allowtransparency="true"
+        ></iframe>
+        <iframe
+          title="Facebook"
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fvitalnortheastern%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=475615013551381"
+          width="340"
+          height="500"
+          style={{ border: "none", overflow: "hidden" }}
+          scrolling="no"
+          frameborder="0"
+          allowfullscreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
       </SocialMediaContainer>
     </SocialMediaSectionContainer>
   )
